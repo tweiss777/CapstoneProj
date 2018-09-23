617c91df82e1b49da1b91b290d96fcadc0936962
Tal Weiss <Tweiss747@gmail.com>
2018-09-23 16:50:34 -0400

    - Need to fix error at line 83. - Getting max retry error.

--- IndeedAPI.py
+++ IndeedAPI.py
@@ -3,7 +3,6 @@ from bs4 import BeautifulSoup
 import urllib3
 import urllib
 import certifi
-
 # vjk=66c82be83c389f01
 # test method that returns a list of jobs
 # limit should be in increments of 10
@@ -26,18 +25,20 @@ def retrieve_urls(query,zipcode,page_limit):
 
         job_links = [a_tags for a_tags in soup.find_all('a', class_='turnstileLink', href=True)]
         for job_link in job_links:
-            total_job_links.append(job_link)
+            total_job_links.append(job_link['href'])
 
         page +=10
 
     for job in total_job_links:
         print(job)
-    print("Total jobs returned %s" % len(total_job_links))
+    print("Total job links returned %s" % len(total_job_links))
     return total_job_links
 
-# tesmethod that returns individual job
-def getJD(query,location,page_limit,job_id):
-    link = "https://indeed.com/company/Source-Infotech-Inc./jobs/Java-Developer-d9cc01025a737866?fccid=25d10be0b57433ae&amp;vjs=3"
+# tesmethod that returns individual job description
+def getJD(href):
+    # link = "https://indeed.com/rc/clk?jk=d2dcaad5873c0ccd&amp;fccid=cad0c703787d24ae&amp;vjs=3"
+
+    link = "https://indeed.com" + href
     http = urllib3.PoolManager(cert_reqs = 'CERT_REQUIRED', ca_certs = certifi.where())
     response = http.request('GET', link)
     html = response.data
@@ -46,6 +47,8 @@ def getJD(query,location,page_limit,job_id):
 
     #return the markup for the specific jd
     print(soup.prettify())
+
+
 
 #test method that returns the job ids
 def getJobIDS(query,location,page_limit):
@@ -70,12 +73,13 @@ def getJobIDS(query,location,page_limit):
         page += 10
 
 
-    return totalJobIds
+    # return totalJobIds
 
 
 def main():
-    retrieve_urls('Java developer', 11590,10)
+    hrefs = retrieve_urls('Java developer', 11590,10)
 
-
+    # Getting max retry error at function call
+    getJD(hrefs[1])
 main()
 
