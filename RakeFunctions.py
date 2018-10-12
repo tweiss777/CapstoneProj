fcb1b1eee5e99352a7a4bff983742ecef09d4e47
Tal Weiss <Tweiss747@gmail.com>
2018-10-12 15:12:35 -0400

    two comments added on lines 6-7

--- RakeFunctions.py
+++ RakeFunctions.py
@@ -3,6 +3,8 @@ from rake_nltk import Rake
 
 class RakeFunctions:
 
+    # function to extract keywords
+    # returns a list of ranked phrases
     def process_rake(self,text):
         r = Rake()
         r.extract_keywords_from_text(text)
