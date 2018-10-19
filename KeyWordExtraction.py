c4e0b5537a65342d3b1ab5fd59ab0c3a69f91678
Tal Weiss <Tweiss747@gmail.com>
2018-10-19 19:21:52 -0400

    nlp pre processing in progress

--- KeyWordExtraction.py
+++ KeyWordExtraction.py
@@ -1,6 +1,8 @@
 # Test file that extracts
+import re
 
 from docx import Document
+from nltk.corpus import stopwords
 
 from IndeedAPI import *
 
@@ -17,7 +19,7 @@ job_urls = indeedApi.retrieve_urls("Java developer",11590,10)
 #this list holds jobs that may have no content.
 unfiltered_jobs = [indeedApi.getJobMarkup(url) for url in job_urls]
 
-#filtered jobs are jobs in list that have content
+# filtered jobs are jobs in list that have content Note: this holds the markup (html) of the job
 filtered_jobs = indeedApi.filterJobs(unfiltered_jobs)
 
 #takes the filtered jobs and creates a dictionary that holds all the jobs
@@ -31,3 +33,20 @@ fullText = []
 
 for p in d.paragraphs:
     fullText.append(p.text)
+
+resumetext = ""  # empty string which should hold the text of the resume
+# iterate through the fullText array and strip \n and \t
+for i in range(len(fullText)):
+    if len(fullText[i] > 0):
+        fullText[i] = re.sub("\s+", " ", fullText[i])  # if there is text remove the extra space and the and the tabs
+        resumetext = resumetext + fullText[i] + " "  # concatinate part of the resume text to the job description
+    else:
+        fullText.pop(i)  # no text? remove it from the list
+
+resumeNoStopWords = [word for word in resumetext.split() if word not in stopwords.words('english')]
+
+# iterate through json_data job title and description and strip the stopwords from the description
+jobsNoStopWords = {}  # job_id: {title: "string", description:
+for i in range(len(json_data)):
+
+# now we will run tf-idf on both the job descriptions and the resume & strip the stopwords
